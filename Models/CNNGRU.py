7795cdaa4c46cc9b7069453867a37d810fd1f110
Alexandre Chartrand <alexchartrand91@gmail.com>
2020-08-28 16:41:07 -0400

    Modification to cnngru

--- Models/CNNGRU.py
+++ Models/CNNGRU.py
@@ -9,16 +9,14 @@ from . import Utility
 # Inspired by: https://arxiv.org/pdf/1712.07108.pdf
 class CNNGRU(nn.Module):
     HIDDEN_SIZE = 512
-    DROPOUT = 0.3
+    DROPOUT = 0.2
     def __init__(self, in_feature, out_feature):
         super(CNNGRU, self).__init__()
-        self.bnx = nn.BatchNorm1d(in_feature)
-        self.convIdentity = Utility.Conv1DSame(in_feature, 64, 1, bias=False)
-        self.res1 = ResBlock(64,64, 5)
-        self.res2 = ResBlock(64, 128, 3)
-        self.res3 = ResBlock(128, 256, 3)
-        self.res4 = ResBlock(256, 512, 3)
-        self.lstm = nn.LSTM(input_size=512, hidden_size=self.HIDDEN_SIZE, num_layers=3, bidirectional=True, batch_first=True, dropout=self.DROPOUT)
+        self.res1 = ResBlock(in_feature,60, 5)
+        self.res2 = ResBlock(60, 120, 3)
+        self.res3 = ResBlock(120, 240, 3)
+        self.res4 = ResBlock(240, 240, 3)
+        self.lstm = nn.LSTM(input_size=240, hidden_size=self.HIDDEN_SIZE, num_layers=3, bidirectional=True, batch_first=True, dropout=self.DROPOUT)
         self.bn1 = nn.BatchNorm1d(self.HIDDEN_SIZE*2)
         self.lin1 = nn.Linear(self.HIDDEN_SIZE*2, self.HIDDEN_SIZE)
         self.bn2 = nn.BatchNorm1d(self.HIDDEN_SIZE)
@@ -33,8 +31,6 @@ class CNNGRU(nn.Module):
                 nn.init.ones_(param)
             elif 'weight' in name:
                 nn.init.uniform_(param, -1/32, 1/32)
-
-        nn.init.ones_(self.convIdentity.conv.weight)
         # Linear
         nn.init.xavier_uniform_(self.lin1.weight)
         nn.init.zeros_(self.lin1.bias)
@@ -42,9 +38,7 @@ class CNNGRU(nn.Module):
         nn.init.zeros_(self.lin2.bias)
 
     def forward(self, x):
-        out = self.bnx(x)
-        out = self.convIdentity(out)
-        out = self.res1(out)
+        out = self.res1(x)
         out = self.res2(out)
         out = self.res3(out)
         out = self.res4(out)
@@ -61,7 +55,7 @@ class CNNGRU(nn.Module):
         return y
 
     def getOptimizer(self):
-        return optim.SGD(self.parameters(), lr=0.1, momentum=0.95, nesterov=True, weight_decay=1e-5)
+        return optim.SGD(self.parameters(), lr=0.1, momentum=0.9, nesterov=True, weight_decay=1e-5)
 
 class DownSampleConv(nn.Module):
 
@@ -85,9 +79,10 @@ class ResBlock(nn.Module):
     def __init__(self, in_feature, out_feature, kernel):
         super(ResBlock, self).__init__()
         self.need_identity = False
-        stride = int(out_feature/in_feature)
+        stride = 1
         if in_feature != out_feature:
             self.need_identity = True
+            stride = 2
             self.convIdentity = Utility.Conv1DSame(in_feature, out_feature, 1, stride=stride, groups=in_feature, bias=False)
 
         self.identityBN = nn.BatchNorm1d(out_feature)
